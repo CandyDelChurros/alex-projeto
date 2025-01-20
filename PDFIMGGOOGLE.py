12d96079951b6a3d71b1c5544a4f110bbf9a5ed8
Tayná <taynaellen1@gmail.com>
2025-01-20 02:25:21 -0300

    iniciando front

--- PDFIMGGOOGLE.py
+++ PDFIMGGOOGLE.py
@@ -1,35 +1,55 @@
+import os
+from flask import Flask, request, render_template, redirect, url_for
 import cv2
 import PIL.Image
 import google.generativeai as genai
 from docx import Document
 
-genai.configure(api_key="insira key")
+app = Flask(__name__)
+
+genai.configure(api_key="AIzaSyAEYr9MEJ4aCLw6z_s9XHEUvtTmLnzLx3M")
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-image_path = "pano.jpg"
-image = cv2.imread(image_path)
+def process_image(image_path):
+    image = cv2.imread(image_path)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 102, 255, cv2.THRESH_BINARY_INV)
+    processed_image_path = "static/img/imagem_processada.png"
+    cv2.imwrite(processed_image_path, thresh)
+    return processed_image_path
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+def generate_docx(text):
+    doc = Document()
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Times New Roman'
+    doc.add_paragraph(text)
+    doc.save("static/img/document.docx")
 
-_, thresh = cv2.threshold(gray, 102, 255, cv2.THRESH_BINARY_INV)
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "image" not in request.files:
+            return redirect(request.url)
+        image_file = request.files["image"]
+        if image_file.filename == "":
+            return redirect(request.url)
+        
+        image_path = os.path.join("static/img", image_file.filename)
+        image_file.save(image_path)
+        
+        processed_image_path = process_image(image_path)
 
-cv2.imwrite("imagem_processada.png", thresh)
+        text_image = PIL.Image.open(processed_image_path)
+        response = model.generate_content(["O que está escrito? apenas transcreva o texto sem comentar nada", text_image])
 
+        generate_docx(response.text)
 
-text = PIL.Image.open("imagem_processada.png")
-response = model.generate_content(["O que está escrito? apenas transcreva o texto sem comentar nada", text])
+        return render_template("img_to_doc.html", document_url=url_for("static", filename="img/document.docx"))
 
-doc = Document()
-style = doc.styles['Normal']
-font = style.font
-font.name = 'Times New Roman'
-text_doc = response.text
+    return render_template("img_to_doc.html")
 
-doc.add_paragraph(text_doc)
-
-doc.save("document.docx")
-
-print("Convertido com sucesso!")
-
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
